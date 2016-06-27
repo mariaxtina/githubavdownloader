19de600a01c6406b77712eb2d5e1268ca02ebbc0
Christina Hess <mchristinahess@gmail.com>
2016-06-27 19:39:18 +0000

    practice using request client

---
+++ request.js
@@ -0,0 +1,11 @@
+var request = require("request");
+
+request("http://www.example.com", function(err, response, body) {
+  if (err) {
+    throw err;
+  }
+
+  console.log(body);
+  console.log("Response Status Code:", response.statusCode);
+
+});
